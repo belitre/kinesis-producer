f8dc2de0e4df989a09d87c2a8aa238b07b5c8e6e
Miguel Angel Terron Santiago <miguel@ouzi.dev>
2020-03-02 14:49:37 +0000

    just change the package to play with it

--- aggregator.go
+++ aggregator.go
@@ -34,7 +34,7 @@ func (a *Aggregator) Put(data []byte, partitionKey string) {
 	// For now, all records in the aggregated record will have
 	// the same partition key.
 	// later, we will add shard-mapper same as the KPL use.
-	// see: https://github.com/a8m/kinesis-producer/issues/1
+	// see: https://github.com/belitre/kinesis-producer/issues/1
 	if len(a.pkeys) == 0 {
 		a.pkeys = []string{partitionKey}
 		a.nbytes += len([]byte(partitionKey))
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/a8m/kinesis-producer
 
 require (
 	github.com/aws/aws-sdk-go v1.21.10
+	github.com/belitre/kinesis-producer v0.2.0
 	github.com/golang/protobuf v1.3.2
 	github.com/jpillora/backoff v0.0.0-20180909062703-3050d21c67d7
 	github.com/pkg/errors v0.8.1 // indirect
@@ -11,3 +12,5 @@ require (
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 )
+
+go 1.13
